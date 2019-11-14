c1ea53bdeed284496a27586f59fc98a1be40cf99
Joao-Nogueira-gh <joaonogueira20@ua.pt>
2019-11-14 17:44:41 +0000

    documentation

--- Golomb.py
+++ Golomb.py
@@ -1,34 +1,58 @@
 import math
 
+#external library that implements truncated binary encoding
 from truncated_binary_encoding import truncated_binary_encoding
 
-#todo: how to calculate optimal M?
-#cant revert truncated binary encoding...
+#TODO: how to calculate optimal M?
+#TODO:cant revert truncated binary encoding...
+
+## \class Golomb 
+# Basic implementation of an encoder/decoder using Golomb Codes
+# \author Tiago Melo 89005
+# \author João Nogueira 89262
 
 class Golomb:
+
     def __init__(self, factor):
+        ## Initialization function
+        # \param[in] factor The value of "M" to be assigned to the Golomb Coder
+        # setting up useful flags for decoding and checking if M==2^x
         self.factor=factor
         self.sequenceFlag=False
         self.standardM=False
         if math.log(factor,2).is_integer():
             self.standardM=True
 
-    def encode(self,string):
-        print('encoding',string)
-        number=int(string)
+
+    ## Encoding function
+    # \param[in] number The number to be encoded
+    # \param[out] code Returns the encoded number (string)
+    # Computing quotient and remainder, and putting them together to form the code
+    # Quotient converted to Unary code
+    # Remainder converted to Binary code 
+    def encode(self,number):
+        print('encoding',number)
+        number=int(number) #in case it's not a number it also works with strings
         (q,r)=divmod(number,self.factor)
         print(q,r)
         un=self.convertToUnary(q)
         bn=self.convertToBinary(r)
-        r=un+bn
-        print('encoded to',r)
-        return r
+        code=un+bn
+        print('encoded to',code, type(code))
+        return code
 
+    ## Decoding function
+    # \param[in] sequence The sequence of 0's and 1's (the code)
+    # \param[out] number Returns decoded number
+    # Iterating sequence:
+    # Quotient is the sum of 1's until a 0 appears
+    # Remainder is the direct conversion of binary code to integer
+    # Number = factor*quotient + remainder
     def decode(self,sequence):
         self.sequenceFlag=False
+        sequence=str(sequence) #making sure it is a string
         q=0
-        r=''
-        #sequence is the encoded string
+        r='' 
         for i in range(0,len(sequence)):
             if sequence[i]=='0' and self.sequenceFlag==False:
                 self.sequenceFlag=True
@@ -40,26 +64,33 @@ class Golomb:
         r=int(r,2)
         print(q,r)
         number=self.factor*q+r
-        print('decoded->',number)
+        print('decoded->',number, type(number))
         return number
 
-
+    ## Convertion to Unary Code
+    # \param[in] number The number to be encoded to Unary
+    # \param[out] sequence The Unary code sequence
+    # In this implementation we add X 1's where X=number, followed by a 0
     def convertToUnary(self,number):
         # x 1's and one 0
-        s=''
+        sequence=''
         for i in range(0,number):
-            s+='1'
-        s+='0'
-        print(s)
-        return s
-    
+            sequence+='1'
+        sequence+='0'
+        print(sequence)
+        return sequence
+    ## Convertion to Binary Code
+    # \param[in] number The number to be encoded to Binary
+    # \param[out] sequence The Binary code sequence
+    # If our factor=2^x then it's normal binary encoding
+    # Else it's truncated binary encoding (WIP)
     def convertToBinary(self,number):
         if self.standardM:
-            conv="{0:b}".format(number) #alternative para retirar 0b
-            print(conv,type(conv))
-            return conv
+            sequence="{0:b}".format(number) #alternative para retirar 0b
+            print(sequence)
+            return sequence
         else:
-            x=truncated_binary_encoding(number,self.factor)
+            sequence=truncated_binary_encoding(number,self.factor)
             # b=math.log(self.factor,2)
             # b=math.ceil(b)
             # if number<b:
@@ -68,5 +99,5 @@ class Golomb:
             # else:
             #     #more bits
             #     pass
-            print("truncated",x,type(x))
-            return x
+            print("truncated",sequence)
+            return sequence
