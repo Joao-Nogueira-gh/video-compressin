3c80662e68d1c8507fdee190028c639a1be8a8c4
Tiago Melo <tleonmelo@hotmail.com>
2019-12-05 18:22:55 +0000

    Tweak

--- src/Video.py
+++ src/Video.py
@@ -62,16 +62,16 @@ class Video:
                 v=np.frombuffer(frameV, dtype=np.uint8)
 
                 y=y.reshape(self.shape)
-                u=u.reshape(self.shape)
-                v=v.reshape(self.shape)
+                u=u.reshape(self.other_shape)
+                v=v.reshape(self.other_shape)
 
                 self.frameY+=[y]
                 self.frameU+=[u]
                 self.frameV+=[v]
                 
-                # print(y, len(y),c, end='\n\n')
-                # print(u, len(u),c, end='\n\n')
-                # print(v, len(v),c, end='\n\n')
+                print(y, len(y),c, end='\n\n')
+                print(u, len(u),c, end='\n\n')
+                print(v, len(v),c, end='\n\n')
 
             c+=1
 
@@ -91,6 +91,7 @@ class Video:
                     self.frameLength=int(self.width*self.height*3)
                     self.yLength=self.uLength=self.vLength=int(self.frameLength/3)
                     self.shape = (int(self.height), self.width)
+                    self.other_shape = (int(self.height), self.width)
                     return
 
                 elif c=='422':
@@ -99,6 +100,7 @@ class Video:
                     self.yLength=int(self.frameLength/2)
                     self.vLength=self.uLength=int(self.frameLength/4)
                     self.shape = (int(self.height), self.width)
+                    self.other_shape = (int(self.height), int(self.width/2))
                     return
                 
                 elif c=='420':
@@ -107,6 +109,7 @@ class Video:
                     self.yLength=int(self.frameLength*(2/3))
                     self.uLength=self.vLength=int(self.frameLength*(1/6))
                     self.shape = (int(self.height), self.width)
+                    self.other_shape = (int(self.height/2), int(self.width/2))
                     return
 
         # If no condition was fullfilled, assume the default   
@@ -115,6 +118,7 @@ class Video:
         self.yLength=int(self.frameLength*(2/3))
         self.uLength=self.vLength=int(self.frameLength*(1/6))
         self.shape = (int(self.height), self.width)
+        self.other_shape = (int(self.height/2), int(self.width/2))
 
     def print(self):
         c=1
@@ -129,3 +133,14 @@ class Video:
         for x in self.frameV:
             print(x, len(x),c, 'v', end='\n\n')
             c+=1
+
+    def resize(self):
+        # Resize arrays
+        if self.colorSpace != '4:4:4':
+
+            self.frameU = cv2.resize(self.frameU, (len(self.frameY), len(self.frameY[0])))
+            self.frameV = cv2.resize(self.frameV, (len(self.frameY), len(self.frameY[0])))
+
+        else:
+
+            print("No resizing needed")
